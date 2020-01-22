cbe4ef19457f4f1fcb4420881237896ce206608f
jDanielDetes <jdanieldetes@gmail.com>
2020-01-22 01:20:01 -0500

    phone-stack implemented

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import React from 'react';
+import React,{Fragment} from 'react';
 import './App.css';
-import Intro from './Components/Intro';
 import 'semantic-ui-css/semantic.min.css'
-import Projects from './Components/Projects';
+import PhoneStack from './Components/PhoneStack';
 
 function App() {
   return (
@@ -11,7 +10,13 @@ function App() {
       <link href="https://fonts.googleapis.com/css?family=Press+Start+2P" rel="stylesheet"/>
     <link href="https://unpkg.com/nes.css/css/nes.css" rel="stylesheet" />
       </head>
-     <Projects/>
+      <Fragment>
+      {/*This React component is perfect for showcasing your mobile app portfolio!
+      ***All that is required to use this in your portfolio is to replace the images in
+      the folder with ones of your choosing, and import src/components/molecules/PhoneStack.js
+      and PhoneStack.css into your project.*/}
+      <PhoneStack />
+    </Fragment>
     </div>
   );
 }
